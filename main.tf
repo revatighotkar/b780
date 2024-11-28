5f213d47d88fe663feefa8d6c6a11d001f229594
Your Name <your.email@example.com>
2024-11-28 22:07:31 +0530

    corrected

--- main.tf
+++ main.tf
@@ -1,13 +1,10 @@
-
-
-provider "aws"{
+provider "aws" {
 region = "us-east-1"
+
 }
-resource "aws_instance" "ec2instance" {
-instance_type =  "t2.micro"
-ami = "ami-0614680123427b75e"
-vpc_security_group_ids =  ["sg-0df0c3e498cb32fbe"]
-  key_name = "reva2511"
-  subnet_id = "subnet-08d3a21d3b3c6e820"
-count = 100
+resource "aws_instance" "ec2" {
+ami="ami-012967cc5a8c9f891"
+instance_type = "t2.large"
+
 }
+
